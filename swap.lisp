c9a023651cd329e9524872e13042c836b076a960
Wilfred Hughes <me@wilfred.me.uk>
2014-09-06 23:31:28 +0100

    Adding a swap implementation in CL.

---
+++ swap.lisp
@@ -0,0 +1,20 @@
+(defmacro swap (x y)
+  (let ((tmp-sym (gensym)))
+    `(let (,tmp-sym)
+       (setf ,tmp-sym ,y)
+       (setf ,y ,x)
+       (setf ,x ,tmp-sym))))
+
+(defun use-swap ()
+  (let ((a 1)
+        (b 2))
+    (swap a b)
+    (format t "a: ~A, b: ~A~%" a b))
+  (let ((my-list (list 3 4)))
+    (swap (first my-list) (second my-list))
+    (format t "(first my-list): ~A, (second my-list): ~A~%"
+            (first my-list) (second my-list))))
+
+(use-swap)
+
+

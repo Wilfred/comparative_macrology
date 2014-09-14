157c1a9d2ad355f490bf8fc4524e9efec903dca8
Wilfred Hughes <me@wilfred.me.uk>
2014-09-14 15:45:24 +0100

    A working swap.clj.

--- swap.clj
+++ swap.clj
@@ -1,14 +1,15 @@
 ;; swap! already exists in Clojure
 (defmacro my-swap! [x y]
-  `(let [tmp# ~x]
-     (set! ~x ~y)
-     (set! ~y tmp#)))
+  `(dosync
+    (let [tmp# @~x]
+      (ref-set ~x @~y)
+      (ref-set ~y tmp#))))
 
-;; Doesn't yet work because of Clojure's immutable-by-default behaviour
-;; http://stackoverflow.com/a/940946/509706
-(let [a 1 b 2]
-  (my-swap! a b)
-  (pr "a")
-  (pr a)
-  (pr "b")
-  (pr b))
+(def a (ref 1))
+(def b (ref 2))
+
+(my-swap! a b)
+(pr "a")
+(pr a)
+(pr "b")
+(pr b)

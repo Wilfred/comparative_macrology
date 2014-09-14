afadfe69e3a8eb2450569dcdd8c4cf9e9ead3f5b
Wilfred Hughes <me@wilfred.me.uk>
2014-09-14 15:34:08 +0100

    Allow arbitrarily many statements in the body in eachit.scm.

--- eachit.scm
+++ eachit.scm
@@ -3,9 +3,10 @@
 (define-syntax each-it
   (lambda (x)
     (syntax-case x ()
-      ((_ lst body)
+      ((_ lst body ...)
        (with-syntax ((it (datum->syntax x 'it)))
-         #'(for-each (lambda (it) body) lst))))))
+         #'(for-each (lambda (it) body ...) lst))))))
 
 (each-it '(1 2 3)
-         (display it))
+         (display it)
+         (display (* it 10)))

b81995374c722bdf491074bf79946239080a3b20
Wilfred Hughes <me@wilfred.me.uk>
2014-09-15 20:33:05 +0100

    Using `mapc` to avoid accidentally using forms that have special meaning to `loop`.

--- eachit.lisp
+++ eachit.lisp
@@ -1,9 +1,8 @@
 ;; Tested on SBCL
 
 (defmacro each-it (list &rest body)
-  `(loop for it in ,list
-      do ,@body))
+  `(mapc (lambda (it) ,@body) ,list))
 
 (each-it (list 1 2 3 4 5)
          (format t "~d~%" it)
-         (format t "~d~%" (+ it 1)))
+         (format t "~d~%" (* it 10)))
